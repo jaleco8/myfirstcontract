ef5d3f74d05390f3a674661d2f329f41f7a79255
Jesus Leon <jesus.leon@linktic.com>
2022-03-01 16:05:52 -0400

    New: Front y Ajustes

---
+++ client/app.js
@@ -0,0 +1,91 @@
+App = {
+  contracts: {},
+  init: async () => {
+    console.log("Loaded");
+    await App.loadEthereum();
+    await App.loudAcount();
+    await App.loadContract();
+    App.render();
+    await App.renderTask();
+  },
+
+  loadEthereum: async () => {
+    if (window.ethereum) {
+      console.log("Ethereum is available");
+      App.web3Provider = window.ethereum;
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+    } else if (window.web3) {
+      web3 = new Web3(window.web3.currentProvider);
+    } else {
+      console.log("Ethereum is not available");
+    }
+  },
+
+  loudAcount: async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    App.account = accounts[0];
+  },
+
+  loadContract: async () => {
+    const res = await fetch("TasksContract.json");
+    const tasksContractJSON = await res.json();
+
+    App.contracts.tasksContract = TruffleContract(tasksContractJSON);
+    App.contracts.tasksContract.setProvider(App.web3Provider);
+    App.tasksContract = await App.contracts.tasksContract.deployed();
+  },
+
+  render: () => {
+    document.getElementById("account").innerText = App.account;
+  },
+
+  renderTask: async () => {
+    const taskCounter = await App.tasksContract.taskCounter();
+    const taskCounterNumber = taskCounter.toNumber();
+    let html = "";
+    for (let i = 1; i <= taskCounterNumber; i++) {
+      const task = await App.tasksContract.tasks(i);
+      const taskId = task[0].toNumber();
+      const taskTitle = task[1];
+      const taskDescription = task[2];
+      const taskDone = task[3];
+      const taskCreated = task[4];
+
+      let taskElement = `
+        <div class="card bg-dark mb-2">
+          <div class="card-header d-flex justify-content-between align-items-center">
+            <span class="card-title">${taskTitle}</span>
+            <div class="form-check form-switch">
+              <input class="form-check-input" data-id="${taskId}" type="checkbox" ${taskDone && "checked"} onChange="App.toggleDone(this)"/>
+            </div>
+          </div>
+          <div class="card-body">
+            <p>${taskDescription}</p>
+            <p class="text-muted">La tarea fue creada ${new Date(taskCreated * 1000).toLocaleString()}</p>
+          </div>
+        </div>
+      `;
+      html += taskElement;
+    }
+    document.querySelector("#tasksList").innerHTML = html;
+  },
+
+  createTask: async (title, description) => {
+    const result = await App.tasksContract.createTask(title, description, {
+      from: App.account
+    });
+    console.log(result.logs[0].args);
+  },
+
+  toggleDone: async (element) => {
+    const taskId = element.dataset.id;
+    await App.tasksContract.toggleDone(taskId, {
+      from: App.account
+    });
+    window.location.reload();
+  }
+};
+
+App.init();
